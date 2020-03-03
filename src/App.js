af2bb162e7f614e493fa5d7807d271c7d49a3099
anchitdhar <anchitdhar@yahoo.com>
2020-03-04 03:04:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      count:0
+    }
+    this.doubleIncrease= this.doubleIncrease.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    this.reduceCount= this.reduceCount.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count:prevState.count + 1
+      }
+    })
+  }
+
+  doubleIncrease() {
+    this.setState(prevState => {
+      return {
+        count:prevState.count + 2
+      }
+    })
+  }
+
+  reduceCount() {
+    this.setState(prevState => {
+      return {
+        count:prevState.count - 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div style={{textAlign:"center"}}>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+        <button onClick={this.doubleIncrease}>DoubleIncrease</button>
+        <button onClick={this.reduceCount}>reduceCount</button>
+      </div>
+    )
+  }
 }
-
 export default App;
